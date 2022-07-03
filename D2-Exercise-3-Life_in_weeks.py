61ac5e051dcb7c1e3c7a5da6f3bd467df07b7df0
Igor Vilembo <vilemboigor@gmail.com>
2022-07-03 21:38:34 +0100

    A program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.

---
+++ D2-Exercise-3-Life_in_weeks.py
@@ -0,0 +1,22 @@
+'''Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+It will take your current age as the input and output a message with our time left in this format:
+
+You have x days, y weeks, and z months left.
+
+Where x, y and z are replaced with the actual calculated numbers.'''
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+time_left = 90 - int(age)
+
+weeks = time_left * round(52.1428571)
+
+days = time_left * 365
+
+months = time_left * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
