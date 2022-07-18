3543bef3b6b587ab566eb9171c58de09e9ec5977
Igor Vilembo <vilemboigor@gmail.com>
2022-07-18 15:15:25 +0100

    A program that calculates the highest score from a List of scores.

--- D5-Exercise1-Average_height.py
+++ D5-Exercise1-Average_height.py
@@ -1,4 +1,4 @@
-#Ask the students height's and  turn them into integers
+#Ask the students height's and turn them into integers
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
---
+++ D5-Exercise2-High_score.py
@@ -0,0 +1,16 @@
+
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+
+#Calculates the highest value
+max_value = 0
+for number in student_scores:
+    if number > max_value:
+        max_value = number
+
+
+print(f"The highest score in the class is: {max_value}.\n")
+
