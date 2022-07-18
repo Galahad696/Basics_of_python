4aa491cd21b564cfbd4f0b2b277a5ef842099839
Igor Vilembo <vilemboigor@gmail.com>
2022-07-18 14:20:36 +0100

    A program that calculates the average student height from a List of heights.

---
+++ D5-Exercise1-Average_height.py
@@ -0,0 +1,17 @@
+#Ask the students height's and them into integers
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+
+
+#Calculate the total elements of the list and the sum
+soma = 0
+total = 0
+for height in student_heights:
+    soma += height
+    total += 1
+#Calculate the average
+media = soma/total
+
+print(round(media))
