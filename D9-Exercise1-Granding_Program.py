2b558165f3eef01fbac0c861ce6d0d3713dac348
Igor Vilembo <vilemboigor@gmail.com>
2022-08-01 23:19:58 +0100

    You have access to a database of student_scores in the format of a dictionary. The keys in student_scores are the names of the students and the values are their exam scores.
    This is a program that converts their scores to grades. By the end of your program, we have a new dictionary called student_grades that should contain student names for keys and their grades for values.

---
+++ D9-Exercise1-Granding_Program.py
@@ -0,0 +1,26 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+
+#Create an empty dictionary called student_grades.
+student_grades = {}
+
+# Add the grades to student_grades.
+for keys in student_scores:
+    score = student_scores[keys]
+    if score >= 91:
+        student_grades[keys] = "Outstanding"
+    elif score >= 81:
+        student_grades[keys] = "Exceeds Expectations"
+    elif score >= 71:
+        student_grades[keys] = "Acceptable"
+    else:
+        student_grades[keys] = "Fail"
+
+
+print(student_grades)
