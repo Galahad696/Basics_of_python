5e4bbd5115892e75de273e57d94051ba779960b5
Igor Vilembo <vilemboigor@gmail.com>
2022-08-05 15:07:41 +0100

    In the starting code, we find the solution from the Leap Year program. First, the function is_leap() is converted so that instead of printing "Leap year." or "Not leap year." it return True if it is a leap year and return False if it is not a leap year. Then create a function called days_in_month() which will take a year and a month as inputs. And it will use this information to work out the number of days in the month, then return that as the output, The List month_days contains the number of days in a month from January to December for a non-leap year. A leap year has 29 days in February.

--- D10-Exercise1-Days_in_Month.py
+++ D10-Exercise1-Days_in_Month.py
@@ -14,7 +14,7 @@ def is_leap(year):
 def days_in_month(chosen_year, chosen_month):
     month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     is_leap(chosen_year)
-    if is_leap(chosen_year) == True:
+    if is_leap(chosen_year):
         month_days[2] = 29
     for i in month_days:
         return month_days[chosen_month]
